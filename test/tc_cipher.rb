20d389f7b6a05baec0def518dbc62e25d4cdd9b2
Technorama team <oss-ruby@technorama.net>
2003-04-24 17:55:49 +0000

    add test to compare output with openssl command line equiv

--- test/tc_cipher.rb
+++ test/tc_cipher.rb
@@ -29,19 +29,37 @@ class TC_Cipher < Test::Unit::TestCase
   def setup
     @c1 = Cipher.new("DES-EDE3-CBC")
     @c2 = DES.new(:EDE3, "CBC")
+
+    @key = "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+    @iv = @key
+    @hexkey = "0000000000000000000000000000000000000000000000"
+    @hexiv = "0000000000000000"
+
+    @data = "DATA"
   end
   def test_crypt
-    key = "KEY"
-    iv = "IV"
-    data = "DATA"
     
-    s1 = @c1.encrypt(key, iv).update(data) + @c1.final
-    s2 = @c2.encrypt(key, iv).update(data) + @c2.final
+    s1 = @c1.encrypt(@key, @iv).update(@data) + @c1.final
+    s2 = @c2.encrypt(@key, @iv).update(@data) + @c2.final
 
     assert_equal(s1, s2, "encrypt")
 
-    assert_equal(data, @c1.decrypt(key, iv).update(s2) + @c1.final, "decrypt")
-    assert_equal(data, @c2.decrypt(key, iv).update(s1) + @c2.final, "decrypt")
+    assert_equal(@data, @c1.decrypt(@key, @iv).update(s2) + @c1.final, "decrypt")
+    assert_equal(@data, @c2.decrypt(@key, @iv).update(s1) + @c2.final, "decrypt")
+  end
+  def test_crypt_openssl_cmdline
+    @c1.encrypt
+    @c1.key = @key
+    @c1.iv = @iv
+    s1 = @c1.update(@data) + @c1.final
+
+    fp = IO::popen("openssl des-ede3-cbc -e -K #{@hexkey} -iv #{@hexiv}", "r+")
+    fp.write(@data)
+    fp.close_write
+    s2 = fp.read
+    fp.close
+
+    assert_equal(s1, s2, "encrypt")
   end
   def test_info
     assert_equal("DES-EDE3-CBC", @c1.name, "name")
