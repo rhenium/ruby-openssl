9ceb30969ebf99b46653ee73b20c06930e66970c
Michal Rokos <m.rokos@sh.cvut.cz>
2003-05-21 19:19:37 +0000

    Version

--- ossl_version.h
+++ ossl_version.h
@@ -11,7 +11,11 @@
 #if !defined(_OSSL_VERSION_H_)
 #define _OSSL_VERSION_H_
 
-/*#define OSSL_VERSION "0.2.0"*/
+/*
+ * Release
+#define OSSL_VERSION "0.2.0-pre2"
+ * is based on
+ */
 #define OSSL_VERSION "OSSL2 - CVS SNAPSHOT ($Date$)"
 
 #endif /* _OSSL_VERSION_H_ */
