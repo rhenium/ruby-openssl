aeabdf72886a54777e23e46a04d8ffe4901a7c9c
Kazuki Yamaguchi <k@rhe.jp>
2024-12-21 02:45:33 +0900

    test/openssl/utils.rb: remove start_immediately from start_server
    The keyword argument is no longer used by any test cases and I do not
    think we will want to use it in the near future.

--- test/openssl/utils.rb
+++ test/openssl/utils.rb
@@ -200,7 +200,7 @@ class OpenSSL::SSLTestCase < OpenSSL::TestCase
     end
   end
 
-  def start_server(verify_mode: OpenSSL::SSL::VERIFY_NONE, start_immediately: true,
+  def start_server(verify_mode: OpenSSL::SSL::VERIFY_NONE,
                    ctx_proc: nil, server_proc: method(:readwrite_loop),
                    accept_proc: proc{},
                    ignore_listener_error: false, &block)
@@ -220,7 +220,6 @@ class OpenSSL::SSLTestCase < OpenSSL::TestCase
       port = tcps.connect_address.ip_port
 
       ssls = OpenSSL::SSL::SSLServer.new(tcps, ctx)
-      ssls.start_immediately = start_immediately
 
       threads = []
       begin
