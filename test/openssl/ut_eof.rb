01c3142b36d472f382099c51e98ded4307542d52
Kazuki Yamaguchi <k@rhe.jp>
2026-09-02 20:19:39 +0900

    xx

--- test/openssl/ut_eof.rb
+++ test/openssl/ut_eof.rb
@@ -7,7 +7,7 @@ module OpenSSL::TestEOF
   def open_file(content)
     ssl_pair { |s1, s2|
       begin
-        th = Thread.new { s2 << content; s2.close }
+        th = Thread.new { s2 << content; s2.close_write }
         yield s1
       ensure
         th&.join
